950443efab1317a062b627430c0ececc8966e1d7
Bawa <7207459+bawasv@users.noreply.github.com>
2024-06-11 17:30:45 -0400

    inheritance

---
+++ cplus19.cpp
@@ -0,0 +1,21 @@
+// **Inheritance** //
+
+#include<iostream>
+#include<string.h>
+#include<iomanip>
+using namespace std;
+
+class Game
+{
+	private:
+		int a[5];
+	public:
+		void setScore(int round, int score)
+		{
+
+		}
+		int getScore(int round)
+		{
+			
+		}
+};
